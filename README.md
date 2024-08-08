b7d34ca376b3a6880e281d70a049b1f66bd04e58
dan <danjedwards01@gmail.com>
2024-08-08 20:41:35 +0100

    Progress with learning python iio api and started work on gui structure.

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ Messing around with the Adalm Pluto.
 * libiio v0.25 C/C++ documentation - https://analogdevicesinc.github.io/libiio/v0.25/libiio/index.html
 * Pluto hardware revisions - https://wiki.analog.com/university/tools/pluto/hacking/hardware#why_do_a_rev_e
 * Update FW https://wiki.analog.com/university/tools/pluto/users/firmware
+* pyadi-iio https://github.com/analogdevicesinc/pyadi-iio
 
 When the Pluto is connected to a host PC, info.html can be opened in the host's file explorer. Lots of helpful information can be found here.
--- src/gui.py
+++ src/gui.py
@@ -1,5 +1,71 @@
-import iio
+import pyqtgraph
+import zmq
 
-ctx = iio.Context("ip:pluto.local")
+from PyQt5.QtCore import (
+    QObject,
+    QThread,
+    pyqtSignal
+)
 
-pass
+from PyQt5.QtWidgets import ( 
+    QApplication,
+    QMainWindow,
+    QWidget,
+    QPushButton
+)
+
+class ZmqWorker(QObject):
+    data = pyqtSignal(bytes)
+    
+    def __init__(self, addr):
+        super().__init__()
+        self.ctx    = zmq.Context()
+        self.socket = self.ctx.socket(zmq.SUB)
+        self.socket.connect(addr)
+        self.running = False
+
+    def run(self):
+        while self.running:
+            buf = self.socket.recv()
+            self.data.emit(buf)
+
+class ZmqWorkerWrapper(QObject):
+    def __init__(self, worker):
+        super().__init__()
+        self.worker = worker
+
+    def start(self):
+        self.thread = QThread()
+        self.worker.moveToThread(self.thread)
+        
+        self.thread.started.connect(self.worker.run)
+        self.worker.data.connect(print)
+
+        self.thread.start()
+
+class PlutoGui(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        # Config
+        self.setWindowTitle("Pluto Gui")
+        
+        # Workers
+        ZmqWorkerWrapper()
+
+        # Widgets
+        btn = QPushButton("Press Me")
+
+        # Signals and Slots
+
+
+        # Layout
+        self.setCentralWidget(btn)
+
+if __name__ == "__main__":
+    import sys
+    
+    app = QApplication(sys.argv)
+    gui = PlutoGui()
+    gui.show()
+    app.exec()
